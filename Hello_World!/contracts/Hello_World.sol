066be2f958603cc66ca2fe099a79db9867b3f0a7
Pauls Toma <tomapt@udmercy.edu>
2025-11-04 17:06:02 -0500

    test1

---
+++ Hello_World!/contracts/Hello_World.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HelloWorld {
+    string private message;
+
+    // Constructor runs once at deployment
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    // Public function to read the stored message
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    // Public function to update the message
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
